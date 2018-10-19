2cc8fba72635dc25deed16119b79338837e37ef4
zhuyadong <aeix@foxmail.com>
2018-10-19 14:40:57 +0800

    window编译兼容debug版

--- prebuild_win-step2.sh
+++ prebuild_win-step2.sh
@@ -2,5 +2,7 @@ basepath=$(cd `dirname $0`; pwd)
 echo "===== prebuild-win-step2 ====="
 cp ../protobuf-cpp/build_win/x86/Release/libprotobuf.lib ./window/x86/
 cp ../protobuf-cpp/build_win/x64/Release/libprotobuf.lib ./window/x86_64/
+cp ../protobuf-cpp/build_win/x86/Debug/libprotobufd.lib ./window/x86/
+cp ../protobuf-cpp/build_win/x64/Debug/libprotobufd.lib ./window/x86_64/
 
 echo "prebuild-win-step2 done"
